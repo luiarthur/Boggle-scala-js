34f44ef0b7ff5187a8bf26bc51e2a7d7d7383cbf
Arthur Lui <luiarthur@gmail.com>
2020-01-14 02:08:33 -0700

    Completed first working version of Boggle.

--- src/main/scala/Boggle/Main.scala
+++ src/main/scala/Boggle/Main.scala
@@ -5,11 +5,93 @@ import scala.scalajs.js
 import js.JSConverters._
 
 import org.querki.jquery._
+import org.querki.jquery
 import org.scalajs.dom
 import dom.document
 
-object Main extends js.JSApp{
+import dom.ext._ // including `Ajax`
+import scala.scalajs
+            .concurrent
+            .JSExecutionContext
+            .Implicits
+            .runNow
+
+object Main extends js.JSApp {
+  val dice = getDice()
+  val dict = getDict()
+  val board = Board(dice, dict)
+
+  def readContent(url: String): String = {
+    var content = ""
+    
+    val ajaxSettings = js.Dynamic.literal(
+      url = url,
+      `type` = "GET",
+      success = (data: String) => content += data,
+      async = false
+    ).asInstanceOf[jquery.JQueryAjaxSettings]
+
+    $.ajax(ajaxSettings)
+
+    return content
+  }
+
+  def getDice() = {
+    val lines = readContent("/assets/txt/dice.txt").trim.split("\n").toVector
+    lines.map(line => Die(line.trim.split(",").toVector))
+  }
+
+  def getDict() = {
+    readContent("/assets/txt/scrabble_dict.txt").trim.split("\n").toVector
+  }
+
+  def setupUI() {
+    $("#root").append("<div id='board' " +
+                      "style='font-family: monospace; font-size: 25'>" + 
+                      "</div>")
+
+    $("#root").append("<div id='solution' " +
+                      "style='font-family: monospace; " +
+                      "font-size: 25; width: 50%'></div>")
+
+    // Add a shuffle button
+    $("#board").append("<button type='button' id='btn-shuffle'>" + 
+                       "Shuffle Board</button>")
+    $("#board").append("<div id='board-grid'><div>")
+    $("#btn-shuffle").click(() => showBoard())
+
+    // Add a show-solution button
+    $("#solution").append("<button type='button' id='btn-solution'>" +
+                          "Show Solution</button>")
+    $("#solution").append("<div id='solution-grid'><div>")
+    $("#btn-solution").click(() => showSolution())
+
+    showBoard()
+  }
+
+  def showBoard() {
+    $("#board-grid").empty()
+    board.shuffle
+    board.letters.vec.foreach{ 
+      row => {
+        val paddedRow = row.map(c => f"$c%2s")
+                           .mkString(" ")
+                           .replace(" ", "&nbsp")
+        $("#board-grid").append(s"<p>${paddedRow}</p>")
+      }
+    }
+    $("#solution-grid").empty()
+  }
+
+  def showSolution() {
+    val solution = board.solve()
+    $("#solution-grid").empty()
+    $("#solution-grid").append(s"<p>${solution.mkString(", ")}</p>")
+  }
+
   def main() = {
-    println("Hello")
+    setupUI()
+
+    println("Let's play Boggle!")
   }
 }
